3370e62353a99f2bd027707d426a1bb3b3687649
oreste-abizera <oresteabizera11@gmail.com>
2022-05-26 00:09:44 +0200

    feat: pydantic schemas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+import schemas
 
 app = FastAPI()
 
@@ -21,4 +22,27 @@ def getItem(id: int):
 def addItem(task: str):
     newId = len(fakeDatabase.keys()) + 1
     fakeDatabase[newId] = {'task': task}
+    return fakeDatabase
+
+# Option # 2
+@app.post("/")
+def addItem(item: schemas.Item):
+    newId = len(fakeDatabase.keys()) + 1
+    fakeDatabase[newId] = {'task': item.task}
+    return fakeDatabase
+
+# OPTION # 3
+# @app.post("/")
+# def addItem(body = Body()):
+#     newId = len(fakeDatabase.keys()) + 1
+#     fakeDatabase[newId] = {'task': body["task"]}
+
+@app.put("/{id}")
+def updateItem(id: int, item: schemas.Item):
+    fakeDatabase[id]["task"] = item.task
+    return fakeDatabase
+
+@app.delete("/{id}")
+def deleteItem(id: int):
+    del fakeDatabase[id]
     return fakeDatabase
